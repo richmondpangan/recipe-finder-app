1315dba72d30012d1ed5a2e495fb183489e7acb4
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-03-17 17:49:54 +0800

    Modified App.jsx
    - Added Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, Link} from "react-router-dom";
+import Search from "./components/Search";
+import styled from "styled-components";
+import {GiKnifeFork} from "react-icons/gi";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={'/'}>Food Recipe</Logo>
+        </Nav>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
     </>
   )
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: Helvetica;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`
 
 export default App
