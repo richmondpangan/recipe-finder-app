30131108c0ecbe753d41ad8da7733eeab37f18c4
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-03-17 17:39:32 +0800

    Added Searched.jsx to pages directory
    - Added a function to fetch data based on the user's search query and
      display it

---
+++ src/pages/Searched.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+function Searched() {
+
+    const [searchedRecipes, setSearchedRecipes] = useState([]);
+    let params = useParams();
+
+    const getSearched = async (name) => {
+
+        const check = localStorage.getItem(name);
+
+        // Check if there is an item in local storage; fetch data if there is nothing and store in local storage
+        if (check) {
+            setCuisine(JSON.parse(check));
+        }
+        else { 
+            const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_REACT_APP_API_KEY}&query=${name}`);
+            const recipes  = await data.json();
+
+            localStorage.setItem(name, JSON.stringify(recipes.results));
+            setSearchedRecipes(recipes.results);
+        }
+    }
+
+    useEffect(() => {
+        getSearched(params.search);
+        console.log(params.search);
+    },[params.search])
+
+  return (
+    <Grid>
+      {searchedRecipes.map((item) => {
+        return(
+            <Card key={item.id}>
+                <Link to={'/recipe/' + item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <h4>{item.title}</h4>
+                </Link>
+            </Card>
+        );
+      })}
+    </Grid>
+  )
+}
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+    img {
+        width: 100%;
+        border-radius: 2rem;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    h4 {
+        text-align: center;
+        padding: 1rem;
+    }
+`;
+
+export default Searched
