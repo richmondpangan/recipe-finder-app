aab3c0c692d4dd5ce64bdfa66966263b76cba47b
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-03-17 17:46:48 +0800

    Modified Cuisine.jsx
    - Added framer-motion

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -31,11 +31,19 @@ function Cuisine() {
     },[params.type])
 
   return (
-    <Grid>
+    <Grid
+        animate={{opacity: 1}}
+        initial={{opacity: 0}}
+        exit={{opacity: 0}}
+        transition={{duration: 0.5}}
+    >
       {cuisine.map((item) => {
         return(
             <Card key={item.id}>
-                <img src={item.image} alt={item.title} />
+                <Link to={'/recipe/' + item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <h4>{item.title}</h4>
+                </Link>
             </Card>
         );
       })}
@@ -43,7 +51,7 @@ function Cuisine() {
   )
 }
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
     grid-gap: 3rem;
