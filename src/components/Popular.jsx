0f66d108449c86522bcd701baf09150f94d90dd4
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-03-21 11:13:58 +0800

    Modified Popular.jsx
    - Changed the fetch number to 10

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -22,7 +22,7 @@ function Popular() {
       setPopular(JSON.parse(check));
     }
     else {
-      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_REACT_APP_API_KEY}&number=9`);
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_REACT_APP_API_KEY}&number=10`);
       const data = await api.json();
 
       localStorage.setItem('popular', JSON.stringify(data.recipes));
