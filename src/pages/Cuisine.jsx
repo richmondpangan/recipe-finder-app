efbd3d15e5eb1acc32b5b95c85e5336cfad443ac
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-03-21 11:12:49 +0800

    Removed commented codes

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import {motion} from 'framer-motion';
 import {Link, useParams} from 'react-router-dom';
 import "../css/Searched.css";
 
@@ -18,7 +16,7 @@ function Cuisine() {
             setCuisine(JSON.parse(check));
         }
         else { 
-            const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_REACT_APP_API_KEY}&cuisine=${name}`);
+            const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_REACT_APP_API_KEY}&cuisine=${name}&number=12`);
             const recipes  = await data.json();
 
             localStorage.setItem(name, JSON.stringify(recipes.results));
@@ -51,27 +49,5 @@ function Cuisine() {
     </div>
   )
 }
-
-/* const Grid = styled(motion.div)`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-    grid-gap: 3rem;
-`;
-
-const Card = styled.div`
-    img {
-        width: 100%;
-        border-radius: 2rem;
-    }
-
-    a {
-        text-decoration: none;
-    }
-
-    h4 {
-        text-align: center;
-        padding: 1rem;
-    }
-`; */
 
 export default Cuisine
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
 import "../css/Searched.css";
 
@@ -18,7 +17,7 @@ function Searched() {
             setSearchedRecipes(JSON.parse(check));
         }
         else { 
-            const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_REACT_APP_API_KEY}&query=${name}`);
+            const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_REACT_APP_API_KEY}&query=${name}&number=12`);
             const recipes  = await data.json();
 
             localStorage.setItem(`search_${name}`, JSON.stringify(recipes.results));
@@ -46,27 +45,5 @@ function Searched() {
     </div>
   )
 }
-
-/* const Grid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-    grid-gap: 3rem;
-`;
-
-const Card = styled.div`
-    img {
-        width: 100%;
-        border-radius: 2rem;
-    }
-
-    a {
-        text-decoration: none;
-    }
-
-    h4 {
-        text-align: center;
-        padding: 1rem;
-    }
-`; */
 
 export default Searched
