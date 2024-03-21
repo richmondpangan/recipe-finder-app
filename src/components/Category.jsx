9d797fd919e78ef723a2a4384549f86961f19422
Richmond <130723287+richmondpangan@users.noreply.github.com>
2024-03-21 11:15:13 +0800

    Removed import styled

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,6 +1,5 @@
 import {FaPizzaSlice, FaHamburger} from "react-icons/fa";
 import {GiNoodles, GiChopsticks} from 'react-icons/gi';
-import styled from "styled-components";
 import {NavLink} from "react-router-dom";
 import "../css/Home.css"
 
